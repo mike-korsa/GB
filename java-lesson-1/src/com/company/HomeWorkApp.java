5e29f040bad2d729e9346f1d60800dcc29537fe2
mike <mike_korsa@mail.ru>
2021-11-04 10:40:25 +0500

    lesson5

---
+++ java-lesson-1/src/com/company/HomeWorkApp.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        // write your code here
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 5;
+        int b = 3;
+    }
+}
