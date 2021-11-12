3c46ec6f73388e9618256be46b27e99bae0129fa
mike <mike_korsa@mail.ru>
2021-11-12 14:19:43 +0500

    lesson-3

--- java-lesson-1/src/com/company/lesson3/Lesson3.java
+++ java-lesson-1/src/com/company/lesson3/Lesson3.java
@@ -5,8 +5,16 @@ import java.util.Arrays;
 public class Lesson3 {
     public static void main(String[] args) {
         //System.out.println(Arrays.toString(  getIntArr(3, 18)));
+        int[] arr = {2, 2, 2, 1, 2, 2,  10, 1};
+        //checkBalance(arr);
+        //printMaxMinValues(arr);
+        int[] arr1 = {1,2,3,4,5,6,7,8,9};
+        printRotatedArray(arr1, 2);
     }
 
+    /**
+     * 1-е задание
+     */
     public static void replaceZeros() {
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int currItem : arr) {
@@ -14,6 +22,9 @@ public class Lesson3 {
         }
     }
 
+    /**
+     * 2-е Задание
+     */
     public static void fillArray() {
         int[] arr = new int[100];
         for (int i = 0; i < arr.length; i++) {
@@ -22,6 +33,7 @@ public class Lesson3 {
     }
 
     /**
+     * 3-е задание
      * Множить на 2, но не все
      */
     public static void multiplyItems() {
@@ -34,6 +46,7 @@ public class Lesson3 {
     }
 
     /**
+     * 4-е задание
      * заменить диагонали в матрице
      */
     public static void peplaceDiagonalElements() {
@@ -48,7 +61,7 @@ public class Lesson3 {
     }
 
     /**
-     *
+     * 5-е задание
      * @param len длина массива
      * @param initialValue чем заполнить
      * @return
@@ -61,18 +74,104 @@ public class Lesson3 {
         return arr;
     }
 
-    public static boolean checkBalance(int[] arr) {
-        for (int i = 1 ; i < arr.length - 1; i++){
-
+    /**
+     * 6-е задание
+     * @param arr
+     */
+    public static void printMaxMinValues(int[] arr) {
+        int minVal = arr[0];
+        int maxVal = arr[0];
+        for (int currItem : arr) {
+            if (currItem < minVal) {
+                minVal = currItem;
+            }
+            if (currItem > maxVal) {
+                maxVal = currItem;
+            }
         }
+        System.out.println(String.format("Массив : %s. Мин. значение - %d, макс. значение - %d",
+                Arrays.toString(arr),minVal,maxVal));
     }
 
+    /**
+     * 7-е задание
+     * @param arr
+     * @return
+     */
+    public static boolean checkBalance(int[] arr) {
+        for (int i = 1 ; i < arr.length; i++){
+            int summLeft = getArrRangeSumm(arr, 0, i - 1);
+            int summRight = getArrRangeSumm(arr, i, arr.length-1);
+            if (summLeft == summRight) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Получить сумму злементов диапазона
+     * @param arr
+     * @param startIndex
+     * @param endIndex
+     * @return
+     */
     public static int getArrRangeSumm(int[] arr, int startIndex, int endIndex) {
         int summValue = 0;
-        for (int i = startIndex ; i <= endIndex - 1; i++){
+        for (int i = startIndex ; i <= endIndex ; i++){
             summValue += arr[i];
         }
         return summValue;
     }
 
+    /**
+     * 8-е задание
+     *
+     */
+    public static void printRotatedArray(int[] arr, int n) {
+        System.out.println(Arrays.toString(arr));
+        for (int j = 1; j <= Math.abs(n); j++){
+            int b;
+            if (n > 0) {
+                int a = arr[arr.length - 1];
+                for (int i = 0 ; i < arr.length; i++) {
+                    b = arr[i];
+                    arr[i] = a;
+                    a = b;
+                }
+            }
+            if (n < 0) {
+                int a = arr[0];
+                for (int i = arr.length -1 ; i >= 0; i--) {
+                    b = arr[i];
+                    arr[i] = a;
+                    a = b;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /*public static void printRotatedArray(int[] arr, int n) {
+        System.out.println(Arrays.toString(arr));
+        if (n > 0) {
+            for (int j = 1; j <= n; j++){
+                int a = arr[0];
+                int b;
+                for (int i = 0 ; i < arr.length-1; i++) {
+                    //двигаем крайние элементы
+                    if (i == 0) {
+                        arr[i] = arr[arr.length - 1];
+                        arr[arr.length - 1] = arr[arr.length - 2];
+                    }
+                    else {
+                        b = arr[i];
+                        arr[i] = a;
+                        a = b;
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }*/
 }
