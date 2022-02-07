b93bca5383458cb83e4633d372e84816d6499946
mike <mike_korsa@mail.ru>
2022-02-07 18:59:49 +0500

    java2 lesson3

--- java-lesson-1/src/com/company/java2/lesson3/Main.java
+++ java-lesson-1/src/com/company/java2/lesson3/Main.java
@@ -2,6 +2,17 @@ package com.company.java2.lesson3;
 
 public class Main {
     public static void main(String[] args) {
-        
+        AddressBook ab = new AddressBook();
+        ab.addPerson(new Person("Иванов","789456"));
+        ab.addPerson(new Person("Иванов","76868768"));
+        ab.addPerson(new Person("Сидоров","23487623"));
+        printPhonesByLastName("Иванов", ab);
+        printPhonesByLastName("Сидоров", ab);
+    }
+
+    static void printPhonesByLastName(String lastName, AddressBook ab) {
+        for (Person p : ab.getListPersonsByLastName(lastName)) {
+            System.out.println("Фамилия: " + p.getLastName() + ", номер: " + p.getPhoneNumber());
+        }
     }
 }
