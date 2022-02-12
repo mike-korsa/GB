bf6348f6c9f1753a86ec85341968a730dfdee366
Mike <mike_korsa@mail.ru>
2022-02-12 12:07:13 +0500

    java2 lesson5

---
+++ java-lesson-1/src/com/company/java2/lesson5/Main.java
@@ -0,0 +1,9 @@
+package com.company.java2.lesson5;
+
+public class Main {
+    static final int size = 10000000;
+    static final int h = size / 2;
+    public static void main(String[] args) {
+        float[] arr = new float[size];
+    }
+}
