523c24f067b89d4f26b1b3850c11129ae5239218
Mike <mike_korsa@mail.ru>
2022-09-29 10:59:56 +0500

    Spring lesson3

--- Spring/src/main/Java/com/company/controllers/ProductController.java
+++ Spring/src/main/Java/com/company/controllers/ProductController.java
@@ -1,6 +1,20 @@
 package com.company.controllers;
 
-public class ProductController {
+import com.company.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-    
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+
+
 }
